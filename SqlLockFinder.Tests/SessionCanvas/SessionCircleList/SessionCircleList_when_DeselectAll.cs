5b8c6d6a345dc3edaaf305af8a5cb0b393136e06
Luc Bos <luc_bos@hotmail.com>
2019-08-30 21:31:50 +0200

    TEST: extra tests for sessionlist

--- SqlLockFinder.Tests/SessionCanvas/SessionCircleList/SessionCircleList_when_DeselectAll.cs
+++ SqlLockFinder.Tests/SessionCanvas/SessionCircleList/SessionCircleList_when_DeselectAll.cs
@@ -1,13 +1,27 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using SqlLockFinder.SessionCanvas;
 
 namespace SqlLockFinder.Tests.SessionCanvas.SessionCircleList
 {
-    public class SessionCircleList_when_DeselectAll
+    public class SessionCircleList_when_DeselectAll: SessionCircleList_TestBase
     {
         [Test]
-        public void TESTNAME()
+        public void It_should_set_the_selected_flag_on_all_circles_to_false()
         {
-            Assert.Fail();
+            var circle1 = new Mock<ISessionCircle>();
+            var circle2 = new Mock<ISessionCircle>();
+            var circle3 = new Mock<ISessionCircle>();
+
+            sessionCircleList.Add(circle1.Object);
+            sessionCircleList.Add(circle2.Object);
+            sessionCircleList.Add(circle3.Object);
+
+            sessionCircleList.DeselectAll();
+
+            circle1.VerifySet(x => x.Selected = false);
+            circle2.VerifySet(x => x.Selected = false);
+            circle3.VerifySet(x => x.Selected = false);
         }
     }
 }
--- SqlLockFinder.Tests/SessionCanvas/SessionCircleList/SessionCircleList_when_NonCollidingPoint.cs
+++ SqlLockFinder.Tests/SessionCanvas/SessionCircleList/SessionCircleList_when_NonCollidingPoint.cs
@@ -1,13 +1,65 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Shapes;
+using FluentAssertions;
+using NUnit.Framework;
+using SqlLockFinder.ActivityMonitor;
+using SqlLockFinder.SessionCanvas;
+using SqlLockFinder.Tests.Util;
 
 namespace SqlLockFinder.Tests.SessionCanvas.SessionCircleList
 {
-    public class SessionCircleList_when_NonCollidingPoint
+    public class SessionCircleList_when_NonCollidingPoint: SessionCircleList_TestBase
     {
         [Test]
-        public void TESTNAME()
+        public void It_should_find_a_place_for_each_circle_without_colliding()
         {
-            Assert.Fail();
+            sessionCircleList.MaxX = 900;
+            sessionCircleList.MaxY = 900;
+
+            var circles = new List<CircleMock>();
+            for (int i = 0; i < 100; i++)
+            {
+                var circle = new CircleMock {Size = Generator.GetRandomNumber(20)};
+                var point = sessionCircleList.NonCollidingPoint(circle.Size);
+                circle.X = point.X;
+                circle.Y = point.Y;
+
+                circles.Add(circle);
+            }
+
+            foreach (var circle in sessionCircleList)
+            {
+                sessionCircleList.Collides(circle).Should().BeFalse();
+            }
+        }
+
+        public class CircleMock: ISessionCircle
+        {
+            public Ellipse Ellipse { get; }
+            public SessionDto Session { get; set; }
+            public object UiElement { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int SpeedX { get; set; }
+            public int SpeedY { get; set; }
+            public DateTime LastSpeedUpdate { get; set; }
+            public int Size { get; set; }
+            public bool Selected { get; set; }
+            public void OnMouseDown(Action<ISessionCircle> action)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Move()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Revert()
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
